e694a21a6284c03cdd8864858ea6b58ba4c19de1
Jbradu2 <138729161+Jbradu2@users.noreply.github.com>
2023-10-01 13:36:42 -0500

    quick fixes
    Added api Key and basic fetch outline to Java Script. 
    Updated html to include a search area.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-var apiKey = 'c564dd883259440db1ec61effce76b34';
-var apiUrl = 'https://api.spoonacular.com/recipes/complexSearch' + apiKey;
+var apiKey = 'c564dd883259440db1ec61effce76b34'
+var apiUrl = 'https://api.spoonacular.com/recipes/complexSearch'
 
 fetch(apiUrl)
   .then(function (response) {
@@ -8,4 +8,4 @@ fetch(apiUrl)
     }
     return response.json();
     //stingify Response?
-  });
+  })
