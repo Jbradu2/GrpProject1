940b348789b595c48d8b3b0ee2db1bd4a50a0f9a
Jbradu2 <138729161+Jbradu2@users.noreply.github.com>
2023-10-03 00:18:20 -0500

    we can now see search results along with title and picture of item!
    Still working with spoonacular to see how to display recipe as I currently do not see it in the array when a search is made

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 //global variables
 var apiKey = '?apiKey=2e2817cf7b4646899c0dd85ef8464be1';
-var apiUrl = 'https://api.spoonacular.com/recipes/complexSearch'
+var apiUrl = 'https://api.spoonacular.com/recipes/complexSearch';
 var searchInput = $('#enterFood');
 var results = $('#results-div');
 var inputText;
@@ -11,7 +11,7 @@ function search(){
   
   if(searchInput.val() !== ""){
     inputText =searchInput.val();
-    //consrturct URL with the input text from user
+    //consrturct variable for URL with the input text from user
 var fetchUrl = apiUrl + apiKey +"&query=" + inputText;
     fetch(fetchUrl)
     .then(function (response) {
@@ -26,6 +26,7 @@ var fetchUrl = apiUrl + apiKey +"&query=" + inputText;
       // Handle the JSON response data here
       // You can update the results div with the data
       console.log(data);
+      displayResults(data.results);
     })
     .catch(function (error) {
       // Handle any other errors that may occur during the fetch
@@ -36,12 +37,38 @@ var fetchUrl = apiUrl + apiKey +"&query=" + inputText;
   results.html("Error Response: No such food");
 }
 }
-$("search-form").submit(function (e) {
+//event listener for 'click' or 'enter key'
+$("#search-form").submit(function (e) {
 e.preventDefault();
 search();
 });
 
-function displayResults(){
+function displayResults(recipeResults) {
+  // Clear previous results
+  results.html('');
+
+  if (recipeResults.length === 0) {
+    results.html('No recipes found.');
+    return;
+  }
+
+  // Loop through the recipe results and display them
+  recipeResults.forEach(function (recipe) {
+    var recipeDiv = $('<div class="recipe">');
+    var title = $('<h2>').text(recipe.title);
+
+    // Check if the API provides an image URL for the item
+    if (recipe.image) {
+      var image = $('<img>').attr('src', recipe.image);
+      recipeDiv.append(image);
+    }
+
+
+    recipeDiv.append(title);
+    results.append(recipeDiv);
+  });
 
 }
-//loop through results
+//given a recipie dashboard with form inputs
+//when i search for a food item
+//then i am presented with image and link to that item with picture
